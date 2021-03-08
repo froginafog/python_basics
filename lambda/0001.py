1037a5933951ffe2ebaca4b8c45b177c8f1eb8ec
froginafog <80227533+froginafog@users.noreply.github.com>
2021-03-08 11:18:39 -0500

    Add files via upload

---
+++ lambda/0001.py
@@ -0,0 +1,24 @@
+f = lambda x: x + 10 #f(x) = x + 10
+
+"""
+def f(x):
+    return x + 10
+"""
+
+g = lambda x: x * x  #f(x) = x * x
+
+"""
+def f(x):
+    return x * x
+"""
+
+x = 5
+print("x:", x)
+print("f(x):", f(x))
+print("g(x):", g(x))
+
+"""
+x: 5
+f(x): 15
+g(x): 25
+"""
---
+++ lambda/0002.py
@@ -0,0 +1,28 @@
+f = lambda x,y: x + y
+
+"""
+def f(x,y):
+    return x + y
+"""
+
+g = lambda x,y : x * y
+
+"""
+def f(x,y):
+    return x * y
+"""
+
+x = 5;
+y = 10;
+
+print("x:", x)
+print("y:", y)
+print("f(x,y):", f(x,y))
+print("g(x,y):", g(x,y))
+
+"""
+x: 5
+y: 10
+f(x,y): 15
+g(x,y): 50
+"""
