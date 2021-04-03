472f639d185995763173f7b41383be89f68a366d
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-04-03 12:24:07 -0400

    Add files via upload

---
+++ statistics/mode.py
@@ -0,0 +1,13 @@
+#The mode is the value that appears most often in a set.
+
+import statistics
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10]
+
+mode = statistics.mode(a)
+
+print("mode:", mode)
+
+"""
+mode: 9
+"""
